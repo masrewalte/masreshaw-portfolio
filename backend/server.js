096890ff2d26802a1b45f8f766eff5b61c95e103
Masre Walte <masrewalte2013@gmail.com>
2025-06-29 16:07:12 +0300

    backend update

--- backend/server.js
+++ backend/server.js
@@ -19,4 +19,6 @@ const PORT = process.env.PORT || 5000;
 app.use("/api/projects", projectRoute);
 app.use("/api/contact", contactRoute);
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, "0.0.0.0", () =>
+  console.log(`Server running on port ${PORT}`)
+);
